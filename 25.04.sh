631c9c28ae3c5d94fa9f6401f344406f901c2a8b
Callum <github@calobyte.net>
2025-03-27 15:00:36 +0000

    cleanup

--- 25.04.sh
+++ 25.04.sh
@@ -1,46 +1,5 @@
 # #!/bin/bash
 
-# # Global Vars
-# OS_VERSION=24.04
-# VERSION=0.2.23
-
-# # Fetch all the named args
-# while [ $# -gt 0 ]; do
-
-#    if [[ $1 == *"--"* ]]; then
-#         v="${1/--/}"
-#         declare $v="$2"
-#    fi
-
-#   shift
-# done
-
-# clear 
-
-# echo "----------------------------------------------------"
-# echo "Welcome to Nubuntu $OS_VERSION (v$VERSION)"
-# echo "=> The following will be installed:"
-# echo " -> debs: $debs"
-# echo " -> flatpaks: $flatpaks"
-# if [ -n "$apt_install" ]; then
-#   echo "=> the following apt install(s) will be invoked"
-#   echo " -> $apt_install"
-# fi
-# if [ -n "$apt_remove" ]; then
-#   echo "=> the following apt remove(s) will be invoked"
-#   echo " -> $apt_remove"
-# fi
-# if [[ $debloat == "yes" ]]; then
-#   echo "=> debloat"
-# fi
-# if [[ $neaten == "yes" ]]; then
-#   echo "=> the shell will also be neatened"
-# fi
-# if [[ $theme == "dark" ]]; then
-#   echo "=> dark theme will be set"
-# fi
-# echo "----------------------------------------------------"
-
 DOWNLOAD_PATH=$HOME/Downloads/tmp
 
 snap_installs="bruno cura-slicer kdenlive onlyoffice-desktopeditors gimp bitwarden dbeaver-ce obs-studio localsend xournalpp"
