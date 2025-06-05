14db586135661637c05bab3e491dd0216dac59d7
Callum <public@calobyte.net>
2025-06-05 12:21:05 +0000

    add caffine

--- 24.04_redux.sh
+++ 24.04_redux.sh
@@ -2,19 +2,18 @@
 
 DOWNLOAD_PATH=$HOME/Downloads/tmp
 
-snap_installs="bruno cura-slicer kdenlive libreoffice gimp bitwarden dbeaver-ce obs-studio localsend xournalpp darktable pinta godot-4 joplin-desktop"
+snap_installs="bruno cura-slicer kdenlive libreoffice gimp bitwarden dbeaver-ce obs-studio localsend xournalpp darktable pinta godot-4 joplin-desktop steam"
 snap_removes="firefox"
 apt_installs="htop aria2 virtualbox tilix vlc git flatpak gnome-software-plugin-flatpak gnome-software-plugin-snap remmina gnome-shell-extensions"
 apt_removes="gnome-user-docs yelp gnome-terminal"
 flatpak_installs="io.gitlab.theevilskeleton.Upscaler io.missioncenter.MissionCenter"
-
-read -p "Ready to install additional extensions? [y/N] " response
 
 gdbus call --session --dest org.gnome.Shell.Extensions --object-path /org/gnome/Shell/Extensions --method org.gnome.Shell.Extensions.InstallRemoteExtension "blur-my-shell@aunetx"
 gdbus call --session --dest org.gnome.Shell.Extensions --object-path /org/gnome/Shell/Extensions --method org.gnome.Shell.Extensions.InstallRemoteExtension "batterytimepercentagecompact@sagrland.de"
 gdbus call --session --dest org.gnome.Shell.Extensions --object-path /org/gnome/Shell/Extensions --method org.gnome.Shell.Extensions.InstallRemoteExtension "batime@martin.zurowietz.de"
 gdbus call --session --dest org.gnome.Shell.Extensions --object-path /org/gnome/Shell/Extensions --method org.gnome.Shell.Extensions.InstallRemoteExtension "Vitals@CoreCoding.com"
 gdbus call --session --dest org.gnome.Shell.Extensions --object-path /org/gnome/Shell/Extensions --method org.gnome.Shell.Extensions.InstallRemoteExtension "Always-Show-Titles-In-Overview@gmail.com"
+gdbus call --session --dest org.gnome.Shell.Extensions --object-path /org/gnome/Shell/Extensions --method org.gnome.Shell.Extensions.InstallRemoteExtension "caffeine@patapon.info"
 
 mkdir $DOWNLOAD_PATH
 
